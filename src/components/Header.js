821dcad6a5c4be2df240253faa7f0dd312d2e51f
ivannaufal7 <ivannaufal7@gmail.com>
2023-02-24 17:56:40 +0700

    update navbar mobile

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Nav from './Nav'
+import NavMobile from './NavMobile';
 import Logo from '../assets/img/logo.png'
 import { Link } from 'react-scroll'
 
@@ -8,10 +9,14 @@ const Header = () => {
   return <header>
     <div className='flex items-center justify-between'>
       <img src={Logo} alt='' />
-      <Nav />
-      <div>
+      <div className='hidden lg:flex'>
+        <Nav />
+      </div>
+      <div className='hidden lg:flex'>
         <a href='mailto:keziadtt@gmail.com' className='btn btn-md font-medium text-orange text-sm lg:text-base bg-orange-100 p-4 cursor-pointer hover:btn-orange hover:text-white'>Contact</a>
       </div>
+
+      <NavMobile />
     </div>
 
   </header>;
--- src/components/NavMobile.js
+++ src/components/NavMobile.js
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+//import menu btn icon
+import { BiMenu } from 'react-icons/bi'
+
+import { Link } from 'react-scroll'
+
+
 
 const NavMobile = () => {
-  return <div>NavMobile</div>;
+  const [isOpen, setIsOpen] = useState(false)
+
+  const handleMenuClick = () => {
+    setIsOpen(false)
+  }
+
+  return <nav className='lg:hidden'>
+    <div onClick={() => setIsOpen(!isOpen)} className='cursor-pointer text-3xl text-heading ml-[10px] lg:hidden'>
+      <BiMenu />
+    </div>
+    <ul className={`${isOpen ? 'max-h-60 p-8' : 'max-h-0 p-0'} flex flex-col absolute w-full bg-white top-24 left-0 shadow-primary space-y-6
+    overflow-hidden transition-all z-50`}>
+      <li>
+        <Link to='home' spy={true} smooth={true} onClick={handleMenuClick} className='cursor-pointer'>Home</Link>
+      </li>
+      <li>
+        <Link to='videos' spy={true} smooth={true} onClick={handleMenuClick} className='cursor-pointer'>Videos</Link>
+      </li>
+      <li>
+        <Link to='collections' spy={true} smooth={true} onClick={handleMenuClick} className='cursor-pointer'>My Collections</Link>
+      </li>
+    </ul>
+  </nav>;
 };
 
 export default NavMobile;
